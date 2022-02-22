d1e03cb3511383254563b7a9a6c4d03145d5e649
aakankshaDev <ash09596@gmail.com>
2022-02-22 10:26:17 +0530

    Simple navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
-      Portfolio App
+      <NavBar />
     </div>
   );
 }
